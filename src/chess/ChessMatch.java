12fd1e3e77feb3bacfe6bdf530b2532c9513eb4c
Eduardo Bif <eduardobif@hotmail.com>
2024-02-27 13:38:49 -0300

    Completed ChessPosition class

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,26 +25,12 @@ public class ChessMatch {
 			}
 		return mat;
 	}
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		
+	}
+	
 	private void initialSetup() {
-		for(int i = 0; i < 8; i++) {
-			board.placePiece(new Pawn(board, Color.WHITE),new Position(1, i));
-			board.placePiece(new Pawn(board, Color.BLACK),new Position(6, i));
-		}
-		board.placePiece(new Rook(board, Color.WHITE),new Position(0, 0));
-		board.placePiece(new Rook(board, Color.WHITE),new Position(0, 7));
-		board.placePiece(new Rook(board, Color.BLACK),new Position(7, 0));
-		board.placePiece(new Rook(board, Color.BLACK),new Position(7, 7));
-		board.placePiece(new Knight(board, Color.WHITE),new Position(0, 6));
-		board.placePiece(new Knight(board, Color.WHITE),new Position(0, 1));
-		board.placePiece(new Knight(board, Color.BLACK),new Position(7, 1));
-		board.placePiece(new Knight(board, Color.BLACK),new Position(7, 6));
-		board.placePiece(new Bishop(board, Color.WHITE),new Position(0, 2));
-		board.placePiece(new Bishop(board, Color.WHITE),new Position(0, 5));
-		board.placePiece(new Bishop(board, Color.BLACK),new Position(7, 2));
-		board.placePiece(new Bishop(board, Color.BLACK),new Position(7, 5));
-		board.placePiece(new King(board, Color.WHITE),new Position(0, 3));
-		board.placePiece(new King(board, Color.BLACK),new Position(7, 3));
-		board.placePiece(new Queen(board, Color.WHITE),new Position(0, 4));
-		board.placePiece(new Queen(board, Color.BLACK),new Position(7, 4));
+		board.placeNewPiece();
 	}
 }
